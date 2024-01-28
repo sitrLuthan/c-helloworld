7a1ac467d05dc4f5e56e9929431e5092a62043a2
Luthan <151644541+sitrLuthan@users.noreply.github.com>
2024-01-28 12:10:05 +0545

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,9 @@
+internal class program
+{
+  public static void Main(string[] args)
+  {
+    Console.Writeline("Name:Luthan Hang Chongbang");
+    Console.Writeline("Roll no.:021336");
+    Console.Writeline("Address:Duwakot, Bhaktapur");
+  }
+}
